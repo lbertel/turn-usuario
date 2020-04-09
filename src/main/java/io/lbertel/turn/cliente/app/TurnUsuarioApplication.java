2c291dc8b4624f16e52819599414b9dec2172182
Luis Bertel <lbertel@gmail.com>
2020-04-09 15:05:35 -0500

    commit inicial

---
+++ src/main/java/io/lbertel/turn/cliente/app/TurnUsuarioApplication.java
@@ -0,0 +1,13 @@
+package io.lbertel.turn.cliente.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurnUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurnUsuarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/lbertel/turn/cliente/app/TurnUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package io.lbertel.turn.cliente.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurnUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
